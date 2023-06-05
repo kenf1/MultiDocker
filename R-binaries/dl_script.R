caf0193faa977058e039de74eaa866d60b56e97b
kenf1 <103538771+kenf1@users.noreply.github.com>
2023-06-05 15:49:53 -0500

    Update actions

--- R-binaries/dl_script.R
+++ R-binaries/dl_script.R
@@ -4,10 +4,11 @@ getwd()
 library(stringr)
 
 binary_urls <- c(
-    "https://github.com/tidyverse/tidyverse/archive/refs/tags/v2.0.0.tar.gz",
     "https://github.com/r-lib/devtools/archive/refs/tags/v2.4.5.tar.gz",
-    "https://github.com/kenf1/kqol/archive/refs/tags/v1.0.0.tar.gz",
-    "https://github.com/jeroen/jsonlite/archive/refs/tags/v1.8.5.tar.gz"
+    "https://github.com/REditorSupport/languageserver/archive/refs/tags/v0.3.15.tar.gz",
+    "https://github.com/jeroen/jsonlite/archive/refs/tags/v1.8.5.tar.gz",
+    "https://github.com/tidyverse/tidyverse/archive/refs/tags/v2.0.0.tar.gz",
+    "https://github.com/kenf1/kqol/archive/refs/tags/v1.0.0.tar.gz"
 )
 
 for (i in seq(length(binary_urls))){
@@ -16,7 +17,7 @@ for (i in seq(length(binary_urls))){
     file_name <- unlist(str_split(url,"/"))
     file_name <- paste(file_name[5],file_name[9],sep="_")
 
-    save_path <- paste("./R-binaries/",file_name)
+    save_path <- paste("./R-binaries/",file_name,sep="")
     download.file(url,save_path)
 }
 
---
+++ Sandbox/actions_install.R
@@ -0,0 +1,5 @@
+#path to tidyverse
+pkg_file <- "../R-binaries/tidyverse_v2.0.0.tar.gz"
+
+#install tar.gz file
+install.packages(pkg_file,repos=NULL,type="source")
