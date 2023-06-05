b72787bd8837657c91907480e19d0276ef1ccae7
kenf1 <103538771+kenf1@users.noreply.github.com>
2023-06-05 16:15:15 -0500

    Update folder path

--- R-binaries/dl_script.R
+++ R-binaries/dl_script.R
@@ -8,6 +8,7 @@ binary_urls <- c(
     "https://github.com/REditorSupport/languageserver/archive/refs/tags/v0.3.15.tar.gz",
     "https://github.com/jeroen/jsonlite/archive/refs/tags/v1.8.5.tar.gz",
     "https://github.com/tidyverse/tidyverse/archive/refs/tags/v2.0.0.tar.gz",
+    "https://github.com/tidyverse/stringr/archive/refs/tags/v1.5.0.tar.gz",
     "https://github.com/kenf1/kqol/archive/refs/tags/v1.0.0.tar.gz"
 )
 
